226f1d28fe1794f3fa70f730eb3bedd39bbe3b7e
Isurpri <hamsisaac@gmail.com>
2026-03-03 12:07:41 +0100

    PatrolPoints/ExitEntryPoints/DustSpawner

---
+++ ROOMBA_2026_STU_noLib/Assets/RoombaWorld/DustSpawner.cs
@@ -0,0 +1,34 @@
+
+using UnityEngine;
+
+public class DustSpawner : MonoBehaviour
+{
+	private GameObject sample;
+
+	public float interval = 5f; // one seed every interval seconds
+
+	private float elapsedTime = 0f; // time elapsed since last generation
+
+	void Start()
+	{
+		sample = Resources.Load<GameObject>("DUST");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		GameObject clone;
+		if (elapsedTime >= interval)
+		{
+			// spawn creating an instance...
+			clone = Instantiate(sample);
+			clone.transform.position = LocationHelper.RandomWalkableLocation();
+			elapsedTime = 0;
+		}
+		else
+		{
+			elapsedTime += Time.deltaTime;
+		}
+
+	}
+}
